050212bcb842f8d742c2bc601d67f6dee1a9fe4a
imade <innar.made@protonmail.com>
2026-09-06 20:00:23 +0700

    Barebones dev env plugins setup

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,74 @@
+return {
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+	"clangd",
+	"gopls",
+	"lua_ls",
+	"rust_analyzer",
+	"tsc",
+      },
+      automatic_enable = true
+    },
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    }
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+    },
+    event = { "InsertEnter", "CmdlineEnter" },
+    config = function()
+      local cmp = require("cmp")
+
+      vim.lsp.config("*", {
+        capabilities = require("cmp_nvim_lsp").default_capabilities(),
+      })
+
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        }),
+        sources = cmp.config.sources(
+	  {
+	    { name = "nvim_lsp" },
+	  },
+	  {
+	    { name = "buffer" },
+	    { name = "path" },
+	  }
+	),
+        snippet = {
+          expand = function(args)
+            vim.snippet.expand(args.body)
+          end,
+        },
+      })
+
+      cmp.setup.cmdline("/", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = { { name = "buffer" } },
+      })
+
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources(
+          { { name = "path" } },
+          { { name = "cmdline" } }
+        ),
+      })
+    end,
+  },
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,53 @@
+return {
+  {
+    -- You also need: cargo binstall tree-sitter-cli
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      local ts = require("nvim-treesitter")
+      ts.setup()
+
+      local langs = {
+	"bash",
+	"c",
+	"cmake",
+	"cpp",
+	"css",
+	"csv",
+	"diff",
+	"dockerfile",
+	"gitignore",
+	"go",
+	"html",
+	"javascript",
+	"json",
+	"lua",
+	"make",
+	"markdown",
+	"markdown_inline",
+	"mermaid",
+	"nginx",
+	"odin",
+	"proto",
+	"python",
+	"rust",
+	"toml",
+	"sql",
+	"tsx",
+	"typescript",
+	"xml",
+	"yaml",
+	"zsh",
+      }
+      ts.install(langs)
+
+      vim.api.nvim_create_autocmd("FileType", {
+	pattern = langs,
+	callback = function()
+	  vim.treesitter.start()
+	end,
+      })
+    end
+  }
+}
